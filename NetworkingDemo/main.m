1964aa0db9e771c51cbacb485ea2434298bbf822
Matthew Holden <jftholden@yahoo.com>
2012-01-30 23:34:38 -0800

    Initial Commit

---
+++ NetworkingDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NetworkingDemo
+//
+//  Created by Matthew Holden on 1/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "mattAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([mattAppDelegate class]));
+	}
+}
---
+++ NetworkingDemo/mattAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  mattAppDelegate.h
+//  NetworkingDemo
+//
+//  Created by Matthew Holden on 1/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mattAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
